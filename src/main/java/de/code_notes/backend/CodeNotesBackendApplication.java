b2f526bd6a2263240f29abd3950566e9a8acb024
flobbe9 <florin735@live.com>
2024-07-21 10:23:23 +0200

    init

---
+++ src/main/java/de/code_notes/backend/CodeNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package de.code_notes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNotesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/code_notes/backend/CodeNotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.code_notes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
